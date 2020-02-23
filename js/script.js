68e619e73c3bd0f679673000a83865de51e81f17
Helena <Lenavas1998@mail.ru>
2020-02-23 14:25:04 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+let money = prompt("Ваш бюджет на месяц?", ""),
+    time = prompt("Введите дату в формате YYYY-MM-DD", "");
+
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false
+};
+
+let a = prompt("Введите обязательную статью расходов в этом месяце"),
+    b = prompt("Во сколько обойдется?"),
+    a1 = prompt("Введите обязательную статью расходов в этом месяце"),
+    b1= prompt("Во сколько обойдется?")
+    
+    appData.expenses[a] = b;
+    appData.expenses[a1] = b1;
+
+    alert(appData.budget / 30);
